ae74efe0fd52123a0c6e9439946c32f737af1a64
Diego Nunez <diego.nunez@ztsystems.com>
2024-01-05 17:51:43 -0500

    Version 11.9
    -Added a file that contains an external variable that can be shared across the directory. The extern variable serves as a flag to indicate if the script ran successfully.

--- src/script_starter.py
+++ src/script_starter.py
@@ -1,9 +1,14 @@
 # Script to start other Python scripts
 
 import os
+import sys
+import extern_vars
 from pathlib import Path
+from datetime import datetime as dt
+from utilities import show_message
+from alerts import *
 
-
+program_start = dt.now()
 main_path = rf"{Path(__file__).parent}{os.sep}main.py"
 
 print("__________________________________________________________________________________________________\n\n")
@@ -15,3 +20,10 @@ print("_________________________________________________________________________
 print("WIP 2nd part: DATA TRANSFORMATION AND LOADING\n_______________________________________\n\n")
 os.system(rf"python {main_path}")
 print("WIP 2nd part: DATA TRANSFORMATION AND LOADING COMPLETED\n_______________________________________\n\n")
+print(f"TOTAL PROGRAM DURATION: {dt.now() - program_start}\n\n")
+
+if extern_vars.success_flag:
+    show_message(AlertType.SUCCESS)
+else:
+    show_message(AlertType.FAILED)
+sys.exit()
